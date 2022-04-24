15c404e4a438648b8c1ce6f8e8dca694b522ec54
lzeio <labham7@gmail.com>
2022-04-24 19:45:42 +0530

    Enemy AI
    Zombie Wanders Around and Look For Player and Created FOV for zombie.

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -9,14 +9,23 @@ public class Zombie : MonoBehaviour
 
     private NavMeshAgent agent;
 
- 
+    public float walkPointRange;
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    
+    public float awarenessDistance;
+    
 
     //Angle
     public float fov = 120f;
+
+
+
     // Start is called before the first frame updat
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+       
     }
 
     // Update is called once per frame
@@ -28,17 +37,29 @@ public class Zombie : MonoBehaviour
         }
         else
         {
+            Wandering();
             SearchForPlayer();
+            
         }
     }
 
     public void SearchForPlayer()
     {
-        if (Vector3.Angle(Vector3.forward, transform.InverseTransformPoint(player.transform.position)) < fov / 2f)
+        Debug.Log("Searching for player");
+        if (Vector3.Angle(Vector3.forward, transform.InverseTransformPoint(player.transform.position)) <= fov / 2f)
         {
-            if (Vector3.Distance(transform.position, player.transform.position) < 10f)
+            if (Vector3.Distance(transform.position, player.transform.position) <= awarenessDistance)
             {
-                OnAware(); //Stopped here for now
+                RaycastHit hit;
+                if (Physics.Linecast(transform.position, player.transform.position, out hit,-1))
+                {
+                    if (hit.transform.CompareTag("Player"))
+                    {
+                        OnAware();
+                    }
+
+                }
+
             }
         }
     }
@@ -50,11 +71,52 @@ public class Zombie : MonoBehaviour
         agent.SetDestination(player.transform.position);
     }
 
+    void WalkPoint()
+    {
+        float randomZ = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+        float randomX = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(randomX, transform.position.y, randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, player.groundMask))
+            walkPointSet = true;
+    }
+
+    void Wandering()
+    {
+        Debug.Log("Wandering");
+        if (!walkPointSet) WalkPoint();
+
+        if (walkPointSet) agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1)
+        {
+            walkPointSet = false;
+        }
+    }
 
     public void OnAware()
     {
         isAware = true;
     }
 
-    // both functions to be declared in player movement since they are player dependent
+
+    
+    private void OnDrawGizmos()
+    {
+        float rayRange = awarenessDistance;
+        float halfFOV = fov / 2f;
+        Quaternion leftRayRotation = Quaternion.AngleAxis(-halfFOV, Vector3.up);
+        Quaternion rightRayRotation = Quaternion.AngleAxis(halfFOV, Vector3.up);
+        Vector3 leftRayDirection = leftRayRotation * transform.forward;
+        Vector3 rightRayDirection = rightRayRotation * transform.forward;
+        Gizmos.DrawRay(transform.position, leftRayDirection * rayRange);
+        Gizmos.DrawRay(transform.position, rightRayDirection * rayRange);
+
+    }
+
+
+    
 }
