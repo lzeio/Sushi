d6f6b83f12e6af8f02d3e0933cda76c66e240bea
lzeio <labham7@gmail.com>
2022-05-18 21:38:41 +0530

    Scripts Changes

--- Assets/Scripts/Destructo.cs
+++ Assets/Scripts/Destructo.cs
@@ -73,7 +73,7 @@ public class Destructo : MonoBehaviour
                 default:
                     break;
             }
-        }
+        }   
     }
 
     void Explosion()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -68,5 +68,6 @@ public class PlayerController : MonoBehaviour
     
 
 
+
 } 
 
