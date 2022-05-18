2aeaefbe3f8ea3347d75c345f51f7968b1f06def
Vicky3252 <83200625+Vicky3252@users.noreply.github.com>
2022-05-18 07:28:55 +0530

    Added Crosshairs
    Added different crosshairs to different guns

--- Assets/UI/Scripts/WeaponSelection.cs
+++ Assets/UI/Scripts/WeaponSelection.cs
@@ -10,21 +10,18 @@ public class WeaponSelection : MonoBehaviour
    {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Debug.Log("Q");
             weapons[0].SetActive(true);
             weapons[1].SetActive(false);
             weapons[2].SetActive(false);
         }
         if(Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Debug.Log("E");
             weapons[0].SetActive(false);
             weapons[1].SetActive(true);
             weapons[2].SetActive(false);
         }
         if(Input.GetKeyDown(KeyCode.Alpha3))
         {
-            Debug.Log("R");
             weapons[0].SetActive(false);
             weapons[1].SetActive(false);
             weapons[2].SetActive(true);
